8631bb5e92486fd2b0e796c03a42bbede4c25656
Sir-Ivysaur <randompsyduck@duck.com>
2022-10-07 21:23:10 +0800

    Added functionality to search.

--- dealwithpkgs.sh
+++ dealwithpkgs.sh
@@ -10,9 +10,10 @@ echo
 sleep 1
 
 # The choice list. Prompts the user to choose between these actions. Once the user has chosen, that choice is written to the $CHOICE variable.
-CHOICE=$(gum choose --height=6 --cursor=">./ " --limit=1 "Install Packages!" "Remove Packages!" "Look for packages!" "Update Packages!" "Add a Repository!" "Exit!")
+CHOICE=$(gum choose --height=6 --cursor="> " --limit=1 "Install Packages!" "Remove Packages!" "Look for packages!" "Update Packages!" "Add a Repository!" "Exit!")
 
 # This reads the variables to see if a specific option was chosen. Then executes the correct script, or a command.
 [[ "$CHOICE" == "Install Packages!" ]] && sleep 1 && ./pkgsinst.sh
 [[ "$CHOICE" == "Remove Packages!" ]] && sleep 1 && ./pkgsrem.sh
+[[ "$CHOICE" == "Look for packages!" ]] && sleep 1 && ./pkgssearch.sh
 [[ "$CHOICE" == "Exit!" ]] && clear && sleep 1 && echo "Okay, then. Have a good day!" && sleep 1 && clear
